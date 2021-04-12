18d548ba089b02a96ae1e2337796e69cf065fbdb
antelk <alojic00@fesb.hr>
2021-04-12 14:28:52 +0200

    2-d integration over rectangular domain

--- code/utils/integrate.py
+++ code/utils/integrate.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 import numpy as np
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, RectBivariateSpline
 
 
 def quad(func, a, b, args=(), n_points=3):
@@ -28,8 +28,37 @@ def quad(func, a, b, args=(), n_points=3):
     if not callable(func):
         raise ValueError('`func` must be callable')
     psi, w = np.polynomial.legendre.leggauss(n_points)
-    x = ((b - a) / 2) * psi + (a + b) / 2
-    return np.sum((b - a) / 2 * w * func(x, *args))
+    xi = ((b - a) / 2) * psi + (a + b) / 2
+    return (b - a) / 2 * w @ func(xi, *args)
+
+
+def dblquad(func, bbox, args=(), n_points=9):
+    """Return the the integral of a given 2-D function, `f(y, x)`,
+    using the Gauss-Legendre quadrature scheme.
+
+    Parameters
+    ----------
+    func : callable
+        integrand
+    a : list or tuple
+        integration domain [min(x), max(x), min(y), max(y)]
+    args : tuple, optional
+        additional arguments for `func`
+    n_points : int, optional
+        degree of the Gauss-Legendre quadrature
+
+    Returns
+    -------
+    float
+        integral of a given function
+    """
+    if not callable(func):
+        raise ValueError('`func` must be callable')
+    psi, w = np.polynomial.legendre.leggauss(n_points)
+    ay, by, ax, bx = bbox
+    xix = (bx - ax) / 2 * psi + (ax + bx) / 2
+    xiy = (by - ay) / 2 * psi + (ay + by) / 2
+    return (bx - ax) / 2 * (by - ay) / 2 * w @ func(xiy, xix, *args) @ w
 
 
 def elementwise_quad(y, x, n_points=3):
@@ -57,7 +86,35 @@ def elementwise_quad(y, x, n_points=3):
         b = x[-1]
     except TypeError:
         print('`x` must be numpy.ndarray')
-    psi, w = np.polynomial.legendre.leggauss(n_points)
-    y_interp = interp1d(x, y, kind='cubic')
-    x_interp = ((b - a) / 2) * psi + (a + b) / 2
-    return np.sum((b - a) / 2 * w * y_interp(x_interp))
+    func = interp1d(x, y, kind='cubic')
+    return quad(func, a, b, n_points=n_points)
+
+
+def elementwise_dblquad(z, x, y, n_points=9):
+    """Return the approximate value of the integral of a given sampled
+    2-D data using the Gauss-Legendre quadrature.
+
+    Parameters
+    ----------
+    z: numpy.ndarray
+        sampled integrand function of shape (x.size, y.size)
+    y : numpy.ndarray
+        y-axis strictly ascending coordinates
+    x : numpy.ndarray
+        x-axis strictly ascending coordinates
+    n_points : int, optional
+        degree of the Gauss-Legendre quadrature
+
+    Returns
+    -------
+    float
+        approximate of the integral of a given function
+    """
+    if not isinstance(y, (np.ndarray, jnp.ndarray)):
+        raise Exception('`y` must be numpy.ndarray.')
+    try:
+        bbox = [y[0], y[-1], x[0], x[-1]]
+    except TypeError:
+        print('Both `x` and `y` must be numpy.ndarray')
+    func = RectBivariateSpline(y, x, z, bbox=bbox)
+    return dblquad(func, bbox, n_points=n_points)
