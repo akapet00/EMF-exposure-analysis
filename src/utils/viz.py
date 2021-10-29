afb42c46d74021a87753ecba6ee91a782b686b15
antelk <alojic00@fesb.hr>
2021-10-29 15:21:18 +0200

    add a function that sets axes in equal ration for 3d plots

--- src/utils/viz.py
+++ src/utils/viz.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def fig_config(latex=False, nrows=1, ncols=1, scaler=1.0, text_size=16,
@@ -58,3 +59,40 @@ def fig_config_reset():
     None
     """
     plt.rcParams.update(plt.rcParamsDefault)
+    
+
+def set_axes_equal(ax):
+    """Return 3-D axes with equal scale.
+    
+    Note: This function is implemented as in https://stackoverflow.com/a/31364297/15005103
+    because matplotlib currently does not support setting ``ax.axis('equal')``
+    for 3-D plotting.
+    
+    Parameters
+    ----------
+    ax : matplotlib.axes._subplots.Axes3DSubplot
+        Axes with 'auto' scale settings.
+    
+    Returns
+    -------
+    matplotlib.axes._subplots.Axes3DSubplot
+        Axes as if the scale settings were defined as 'equal'.
+    """
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # bounding box is a sphere in the sense of the infinity norm
+    plot_radius = 0.5 * max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+    return ax
