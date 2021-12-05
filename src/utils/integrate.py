2292a94832706eeaa1c8812e9a5269214385ba4a
antelk <alojic00@fesb.hr>
2021-12-05 17:22:09 +0100

    update double integration of a sampled function

--- src/utils/integrate.py
+++ src/utils/integrate.py
@@ -2,11 +2,11 @@ import collections
 
 import jax.numpy as jnp
 import numpy as np
-from scipy.interpolate import interp1d, RectBivariateSpline
+from scipy import interpolate
 from scipy.special import roots_legendre
 
 
-def quad(func, a, b, args=(), n_points=3):
+def quad(func, a, b, args=(), leggauss_deg=3):
     """Return the the integral of a given function using the
     Gauss-Legendre quadrature scheme.
 
@@ -20,7 +20,7 @@ def quad(func, a, b, args=(), n_points=3):
         Right boundary of the integration domain.
     args : tuple, optional
         Additional arguments for `func`.
-    n_points : int, optional
+    leggauss_deg : int, optional
         Degree of the Gauss-Legendre quadrature.
 
     Returns
@@ -30,24 +30,26 @@ def quad(func, a, b, args=(), n_points=3):
     """
     if not callable(func):
         raise ValueError('`func` must be callable')
-    x, w = roots_legendre(n_points)
-    xi = (b - a) / 2 * x + (a + b) / 2
-    return (b - a) / 2 * w @ func(xi, *args)
+    points, w = roots_legendre(leggauss_deg)
+    scaler = (b - a) / 2
+    x = scaler * (points + 1.) + a
+    I_num = (scaler * w) @ func(x, *args)
+    return I_num
 
 
-def dblquad(func, bbox, args=(), n_points=9):
-    """Return the the integral of a given 2-D function, `f(y, x)`,
+def dblquad(func, bbox, args=(), leggauss_deg=9):
+    """Return the the integral of a given 2-D function, `f(x, y)`,
     using the Gauss-Legendre quadrature scheme.
 
     Parameters
     ----------
     func : callable
         Integrand function.
-    a : list or tuple
+    bbox : list or tuple
         Integration domain [min(x), max(x), min(y), max(y)].
     args : tuple, optional
         Additional arguments for `func`.
-    n_points : int, optional
+    leggauss_deg : int, optional
         Degree of the Gauss-Legendre quadrature.
 
     Returns
@@ -57,67 +59,124 @@ def dblquad(func, bbox, args=(), n_points=9):
     """
     if not callable(func):
         raise ValueError('`func` must be callable')
-    x, w = roots_legendre(n_points)
-    ay, by, ax, bx = bbox
-    xix = (bx - ax) / 2 * x + (ax + bx) / 2
-    xiy = (by - ay) / 2 * x + (ay + by) / 2
-    return (bx - ax) / 2 * (by - ay) / 2 * w @ func(xiy, xix, *args) @ w
+    points, w = roots_legendre(leggauss_deg)
+    x_a, x_b, y_a, y_b = bbox
+    x_scaler = (x_b - x_a) / 2
+    y_scaler = (y_b - y_a) / 2
+    x_scaled = x_scaler * (points + 1.) + x_a
+    y_scaled = y_scaler * (points + 1.) + y_a
+    X, Y = np.meshgrid(x_scaled, y_scaled)
+    I_num = (x_scaler * w) @ func(X, Y, *args) @ (y_scaler * w)
+    return I_num
 
 
-def elementwise_quad(y, x, n_points=3):
+def elementwise_quad(points, values, leggauss_deg=3, interp_func=None,
+                     **kwargs):
     """Return the approximate value of the integral of a given sampled
     data using the Gauss-Legendre quadrature.
 
     Parameters
     ----------
-    y : numpy.ndarray
-        Sampled integrand.
-    x : numpy.ndarray
+    points : numpy.ndarray
         Integration domain.
-    n_points : int, optional
+    values : numpy.ndarray
+        Sampled integrand.
+    leggauss_deg : int, optional
         Degree of the Gauss-Legendre quadrature.
+    interp_func : callable, optional
+        Interpolation function.
+    kwargs : dict, optional
+        Additional keyword arguments for interpolation function
 
     Returns
     -------
     float
         Approximation of the integral of a given function.
     """
-    if not isinstance(y, (collections.Sequence, jnp.ndarray, np.ndarray, )):
+    if not isinstance(values, (collections.Sequence, jnp.ndarray, np.ndarray)):
         raise Exception('`y` must be array-like.')
     try:
-        a = x[0]
-        b = x[-1]
+        a = points[0]
+        b = points[-1]
     except TypeError:
         print('`x` must be array-like')
-    func = interp1d(x, y, kind='cubic')
-    return quad(func, a, b, n_points=n_points)
+    if interp_func is None:
+        func = interpolate.interp1d(points, values, **kwargs)
+    else:
+        func = interp_func(points, values, **kwargs)
+    return quad(func, a, b, leggauss_deg=leggauss_deg)
 
 
-def elementwise_dblquad(z, x, y, n_points=9):
+def elementwise_dblquad(points, values, leggaus_deg=9, interp_func=None,
+                        **kwargs):
     """Return the approximate value of the integral of a given sampled
     2-D data using the Gauss-Legendre quadrature.
 
     Parameters
     ----------
-    z: numpy.ndarray
-        Sampled integrand function of shape (x.size, y.size)
-    y : numpy.ndarray
-        y-axis strictly ascending coordinates.
-    x : numpy.ndarray
-        x-axis strictly ascending coordinates.
-    n_points : int, optional
+    points : numpy.ndarray
+        Data point coordinates of shape (n, D).
+    values : numpy.ndarray
+        Sampled integrand function values of shape (n, ). If the data
+        is sampled over a grid data, it could also be of shape (m, m),
+        where m corresponds to the number of data points coordinates.
+    leggauss_deg : int, optional
         Degree of the Gauss-Legendre quadrature.
+    interp_func : callable, optional
+        Interpolation function. If not set radial basis function
+        interpolation is used.
+    kwargs : dict, optional
+        Additional keyword arguments for interpolation function.
 
     Returns
     -------
     float
         Approximation of the integral of a given function.
     """
-    if not isinstance(y, (collections.Sequence, jnp.ndarray, np.ndarray, )):
+    if not isinstance(values, (collections.Sequence, jnp.ndarray, np.ndarray)):
         raise Exception('`y` must be array-like.')
     try:
-        bbox = [y[0], y[-1], x[0], x[-1]]
+        bbox = [points[:, 0].min(), points[:, 0].max(),
+                points[:, 1].min(), points[:, 1].max()]
     except TypeError:
         print('Both `x` and `y` must be arrays')
-    func = RectBivariateSpline(y, x, z, bbox=bbox)
-    return dblquad(func, bbox, n_points=n_points)
+    if interp_func is None:
+        func = interpolate.Rbf(points[:, 0], points[:, 1], values, **kwargs)
+    else:
+        func = interp_func(points[:, 0], points[:, 1], values, **kwargs)
+    return dblquad(func, bbox, leggauss_deg=leggaus_deg)
+
+
+def elementwise_rectquad(x, y, values, leggauss_deg=9, **kwargs):
+    """Return the approximate value of the integral of a given sampled
+    2-D data over a rectangular grid using the Gauss-Legendre
+    quadrature.
+
+    Parameters
+    ----------
+    x : numpy.ndarray
+        x-axis strictly ascending coordinates.
+    y : numpy.ndarray
+        y-axis strictly ascending coordinates.
+    values: numpy.ndarray
+        Sampled integrand function of shape (x.size, y.size)
+    leggauss_deg : int, optional
+        Degree of the Gauss-Legendre quadrature.
+    kwargs : dict, optional
+        Additional keyword arguments for
+        `scipy.interpolate.RectBivariateSpline` function.
+
+    Returns
+    -------
+    float
+        Approximation of the integral of a given function.
+    """
+    if not isinstance(values, (collections.Sequence, jnp.ndarray, np.ndarray)):
+        raise Exception('`values` must be array-like.')
+    try:
+        bbox = [x.min(), x.max(), y.min(), y.max()]
+    except TypeError:
+        print('Both `x` and `y` must be arrays')
+    func = interpolate.RectBivariateSpline(x, y, values, bbox=bbox, **kwargs)
+    I_approx = func.integral(*bbox)
+    return I_approx
